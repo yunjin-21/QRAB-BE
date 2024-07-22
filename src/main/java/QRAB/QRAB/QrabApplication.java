8d079cc8a15f8c02db1698c2c145ba881fdb6b81
leeyunjin <yuncom21@ewhain.net>
2024-07-23 00:02:44 +0900

    초기 세팅

---
+++ src/main/java/QRAB/QRAB/QrabApplication.java
@@ -0,0 +1,13 @@
+package QRAB.QRAB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/QRAB/QRAB/QrabApplicationTests.java
@@ -0,0 +1,13 @@
+package QRAB.QRAB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
