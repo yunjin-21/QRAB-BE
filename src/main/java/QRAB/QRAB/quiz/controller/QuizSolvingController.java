e037bc2a821f708af579f728aa29f2818329fea3
MinyoungK <ddorogi00@gmail.com>
2024-11-03 01:55:59 +0900

    feat: 퀴즈 풀기 퀴즈 조회 기능 생성
    - 퀴즈 풀기 페이지에서 quizSetId를 통해 해당 quizSet의 노트 제목과 퀴즈 관련 정보를 조회하도록 구현

--- src/main/java/QRAB/QRAB/quiz/controller/QuizSolvingController.java
+++ src/main/java/QRAB/QRAB/quiz/controller/QuizSolvingController.java
@@ -1,30 +1,39 @@
 package QRAB.QRAB.quiz.controller;
 
+import QRAB.QRAB.quiz.dto.QuizSolvingResponseDTO;
 import QRAB.QRAB.quiz.dto.UnsolvedQuizSetResponseDTO;
 import QRAB.QRAB.quiz.service.QuizService;
+import QRAB.QRAB.quiz.service.QuizSolvingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/quiz-solving")
 public class QuizSolvingController {
 
     private final QuizService quizService;
+    private final QuizSolvingService quizSolvingService;
 
     @Autowired
-    public QuizSolvingController(QuizService quizService) {
+    public QuizSolvingController(QuizService quizService, QuizSolvingService quizSolvingService) {
         this.quizService = quizService;
+        this.quizSolvingService = quizSolvingService;
     }
 
+    // 퀴즈 풀기 전체 퀴즈세트 조회
     @GetMapping
     public ResponseEntity<Page<UnsolvedQuizSetResponseDTO>> findUnsolvedQuizSets(
             @RequestParam(name = "page", defaultValue = "0") int page) {
         Page<UnsolvedQuizSetResponseDTO> unsolvedQuizSets = quizService.findUnsolvedQuizSets(page);
         return ResponseEntity.ok(unsolvedQuizSets);
+    }
+
+    // 퀴즈 풀기 내 퀴즈 조회
+    @GetMapping("/{quizSetId}/quizzes")
+    public ResponseEntity<QuizSolvingResponseDTO> getQuizSetDetails(@PathVariable Long quizSetId) {
+        QuizSolvingResponseDTO response = quizSolvingService.getQuizSetDetails(quizSetId);
+        return ResponseEntity.ok(response);
     }
 }
