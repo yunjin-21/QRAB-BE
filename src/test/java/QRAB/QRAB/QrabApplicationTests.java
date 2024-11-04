e5b773bbd2b9b1d034639e6cae52be1627391f65
leeyunjin <yuncom21@ewhain.net>
2024-11-04 23:52:49 +0900

    feat : CI 기능 추가

--- src/test/java/QRAB/QRAB/QrabApplicationTests.java
+++ src/test/java/QRAB/QRAB/QrabApplicationTests.java
@@ -1,4 +1,4 @@
-package QRAB.QRAB;
+/*package QRAB.QRAB;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class QrabApplicationTests {
 	}
 
 }
+*/
