0ecfe5ea18a3f014a4619b27db58244ff54ce713
MinyoungK <ddorogi00@gmail.com>
2024-11-05 19:47:37 +0900

    feat: 오답 복습 퀴즈 조회 기능 생성
    - 사용자가 틀린 문제를 다시 풀어볼 수 있도록 quizSetId를 통해 오답 복습 퀴즈를 조회하는 기능 구현

--- src/main/java/QRAB/QRAB/quiz/repository/QuizAnswerRepository.java
+++ src/main/java/QRAB/QRAB/quiz/repository/QuizAnswerRepository.java
@@ -3,6 +3,8 @@ package QRAB.QRAB.quiz.repository;
 import QRAB.QRAB.quiz.domain.QuizAnswer;
 import QRAB.QRAB.quiz.domain.QuizResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,11 @@ import java.util.List;
 @Repository
 public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Long> {
     List<QuizAnswer> findByQuizResult(QuizResult quizResult);
+    // quizSetId로 오답 조회
+    @Query("SELECT qa FROM QuizAnswer qa " +
+            "JOIN qa.quiz q " +
+            "JOIN q.quizSet qs " +
+            "WHERE qs.quizSetId = :quizSetId AND qa.isCorrect = false")
+    List<QuizAnswer> findIncorrectAnswersByQuizSetId(@Param("quizSetId") Long quizSetId);
 }
 
