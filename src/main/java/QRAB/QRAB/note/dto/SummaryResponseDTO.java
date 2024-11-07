1e77db51e52bd829c1e1bd7acb2131ddecd4388d
leeyunjin <yuncom21@ewhain.net>
2024-11-08 08:14:23 +0900

    feat : 특정 노트요약본 조회에서 noteId 반환 추가

--- src/main/java/QRAB/QRAB/note/dto/SummaryResponseDTO.java
+++ src/main/java/QRAB/QRAB/note/dto/SummaryResponseDTO.java
@@ -7,12 +7,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class SummaryResponseDTO {
+    private Long noteId;
     private String title;
     private String chatgptContent;
     private String categoryName;
     private String parentCategoryName;
 
     public static SummaryResponseDTO fromEntity(Note note){
-        return new SummaryResponseDTO(note.getTitle(), note.getChatgptContent(), note.getCategory().getName() , note.getCategory().getParentCategory() != null ? note.getCategory().getParentCategory().getName() : "");
+        return new SummaryResponseDTO(note.getId(), note.getTitle(), note.getChatgptContent(), note.getCategory().getName() , note.getCategory().getParentCategory() != null ? note.getCategory().getParentCategory().getName() : "");
     }
 }
