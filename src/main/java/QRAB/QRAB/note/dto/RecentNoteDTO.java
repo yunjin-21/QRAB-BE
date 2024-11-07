b3a056751e61de42a8e87e7dea3bdf5eecab0961
leeyunjin <yuncom21@ewhain.net>
2024-11-07 11:59:05 +0900

    feat : 최근 노트 조회에서 각 노트의 상위 카테고리 정보 포함

--- src/main/java/QRAB/QRAB/note/dto/RecentNoteDTO.java
+++ src/main/java/QRAB/QRAB/note/dto/RecentNoteDTO.java
@@ -12,6 +12,7 @@ public class RecentNoteDTO {
     private Long noteId;
     private String title;
     private String categoryName; //현재 카테로 이름을 가져옴! (부모 or 자식 일 수 도 )
+    private String parentCategoryName;
     private LocalDateTime createdAt;
     private String chatgptContent;
 
@@ -20,6 +21,7 @@ public class RecentNoteDTO {
                 note.getId(),
                 note.getTitle(),
                 note.getCategory().getName(),
+                note.getCategory().getParentCategory() != null ? note.getCategory().getParentCategory().getName() : "",
                 note.getCreatedAt(),
                 note.getChatgptContent().length() > 250 ? note.getChatgptContent().substring(0, 250) : note.getChatgptContent());
     }
